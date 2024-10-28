a77142b16c35c333e20c6daab673943ea290534e
AK <herogupta626@gmail.com>
2024-10-28 21:31:19 +0530

    C Solution
    Answers to all the C question in Hackkerrank

---
+++ C/Sum and Difference of Two Numbers/Sum and Difference of Two Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int x,y;
+    float a,b;
+    scanf("%d %d",&x,&y) ; 
+    scanf("%f %f" , &a,&b) ; 
+    printf("%d %d\n",x+y,x-y) ;
+    printf("%.1f %.1f" , a+b,a-b) ;
+    return 0;
+}
